52a4719c506befe33ecf3838e5c823389df320c9
janakirama008 <janakirama008@gmail.com>
2025-09-08 12:27:43 +0530

    db-sqlite3 is removed, apps/customer/views.py modified

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -10,37 +10,43 @@ from django.contrib.auth import login, get_user_model
 
 User = get_user_model()
 
+
 class OTPLoginView(FormView):
-    template_name = 'oscar/customer/login.html'
+    template_name = "oscar/customer/login.html"
     form_class = PhoneOTPForm
+
     def form_valid(self, form):
-        phone = form.cleaned_data['phone']
-        otp = form.cleaned_data.get('otp')
-        print('entered form valid')
-        if 'resend' in self.request.POST:
+        phone = form.cleaned_data["phone"]
+        otp = form.cleaned_data.get("otp")
+        print("entered form valid")
+        if "resend" in self.request.POST:
             new_otp = str(random.randint(100000, 999999))
-            cache.set(f'otp_{phone}', new_otp, timeout=300)
+            cache.set(f"otp_{phone}", new_otp, timeout=300)
             send_sms_via_fast2sms(phone, new_otp)
             messages.info(self.request, f"New OTP sent to {phone}")
             return self.render_to_response(self.get_context_data(form=form))
-            
+
         if not otp:
             generated = str(random.randint(100000, 999999))
-            cache.set(f'otp_{phone}', generated, timeout=300)
-            print('OTP: ',cache.get(f'otp_{phone}'))
+            cache.set(f"otp_{phone}", generated, timeout=300)
+            print("OTP: ", cache.get(f"otp_{phone}"))
             # send_sms_via_fast2sms(phone, generated)
             messages.info(self.request, f"OTP sent to {phone} is {generated}")
             # self.template_name = 'oscar/customer/verify_otp.html'
-            return render(self.request, self.template_name , self.get_context_data(form=form, is_otp_sent=True))
+            return render(
+                self.request,
+                self.template_name,
+                self.get_context_data(form=form, is_otp_sent=True),
+            )
 
-        if cache.get(f'otp_{phone}') == otp:
-            print('otp matched')
+        if cache.get(f"otp_{phone}") == otp:
+            print("otp matched")
             user, _ = User.objects.get_or_create(phone=phone)
             login(self.request, user)
-            return redirect('/')
+            return redirect("/oscar/checkout/shipping-address/")
         else:
-            print('otp not matched')
-            form.add_error('otp', 'Invalid OTP')
+            print("otp not matched")
+            form.add_error("otp", "Invalid OTP")
             return self.form_invalid(form)
 
 
@@ -48,46 +54,52 @@ from oscar.apps.customer.views import ProfileView as CoreProfileView
 from oscar.apps.customer.views import ProfileUpdateView as CoreProfileUpdateView
 from rewards.models import Wallet
 
+
 class ProfileView(CoreProfileView):
 
     def get_profile_fields(self, user):
         field_data = super().get_profile_fields(user)
-        
+
         try:
             wallet = Wallet.objects.get(user=user)
-            field_data.extend([
-                {
-                    'name': 'Wallet Balance',
-                    'value': wallet.balance,
-                    'is_currency': True  # Custom flag for template
-                }
-            ])
+            field_data.extend(
+                [
+                    {
+                        "name": "Wallet Balance",
+                        "value": wallet.balance,
+                        "is_currency": True,  # Custom flag for template
+                    }
+                ]
+            )
         except Wallet.DoesNotExist:
             print("Wallet does not exist for user:", user)
             pass
-            
+
         return field_data
-    
-    
+
+
 class ProfileUpdateView(CoreProfileUpdateView):
     form_class = ProfileForm  # Use the custom UserForm defined in forms.py
 
 
 from django.views import generic
+
+
 class ReferredListView(generic.ListView):
     template_name = "oscar/customer/profile/referred.html"
+
     # context_object_name = "my_referred"
     def get_queryset(self):
         return User.objects.filter(referred_by=self.request.user)
+
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get the default context
         context = super().get_context_data(**kwargs)
-        
+
         # Add your custom context data
         my_referred = self.get_queryset()
-        context['page_title'] = 'Referred By Me'
-        context['my_referred'] = my_referred
-        context['count'] = my_referred.count()
-        
-        return context
+        context["page_title"] = "Referred By Me"
+        context["my_referred"] = my_referred
+        context["count"] = my_referred.count()
 
+        return context
