7ee8cdb766d70293b982bf286bba6992b9e71c3f
janakirama008 <janakirama008@gmail.com>
2026-06-26 13:21:07 +0530

    changed to bazaro template

--- virginpreps/settings/base.py
+++ virginpreps/settings/base.py
@@ -88,7 +88,7 @@ ROOT_URLCONF = "virginpreps.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates" / "bazaro"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
